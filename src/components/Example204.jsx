9579765aa6c6a75be6cd988f071e2b9455f0d83e
Andrew D <56039221+andentx@users.noreply.github.com>
2021-12-03 15:08:11 -0500

    Add example 2.04

---
+++ src/components/Example204.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class NumberInformation extends Component {
+    state = {
+        x: 11,
+        y: 22,
+    };
+
+    isPrime = (number) => {
+        for (let i = 2; i < number; i++) {
+            if (number % i === 0) return false;
+        }
+        return true;
+    };
+
+    render() {
+        let { x, y } = this.state;
+        return (
+            <React.Fragment>
+                <p>
+                    X is {x} Prime is {"" + this.isPrime(x)}
+                </p>
+                <p>
+                    Y is {y} Prime is {"" + this.isPrime(y)}
+                </p>
+                <p>Sum is {x + y}</p>
+                <p>Greater is {x > y ? x : y} </p>
+            </React.Fragment>
+        );
+    }
+}
+
+export default NumberInformation;
--- src/components/ExampleList.jsx
+++ src/components/ExampleList.jsx
@@ -2,24 +2,29 @@ import React from "react";
 import Example201 from "./Example201";
 import Example202 from "./Example202";
 import Example203 from "./Example203";
+import Example204 from "./Example204";
 import ExampleContainer from "./ExampleContainer";
 
 import { exampleList } from "./ExampleList.module.css";
 
 export default function ExampleList() {
-  return (
-    <div className={exampleList}>
-      <ExampleContainer exampleTitle={"2.01 - Hello World"}>
-        <Example201 />
-      </ExampleContainer>
+    return (
+        <div className={exampleList}>
+            <ExampleContainer exampleTitle={"2.01 - Hello World"}>
+                <Example201 />
+            </ExampleContainer>
 
-      <ExampleContainer exampleTitle={"2.02 - React Definition"}>
-        <Example202 />
-      </ExampleContainer>
+            <ExampleContainer exampleTitle={"2.02 - React Definition"}>
+                <Example202 />
+            </ExampleContainer>
 
-      <ExampleContainer exampleTitle={"2.03 - Three Strings"}>
-        <Example203 />
-      </ExampleContainer>
-    </div>
-  );
+            <ExampleContainer exampleTitle={"2.03 - List of Strings"}>
+                <Example203 />
+            </ExampleContainer>
+
+            <ExampleContainer exampleTitle={"2.04 - Number Information"}>
+                <Example204 />
+            </ExampleContainer>
+        </div>
+    );
 }
